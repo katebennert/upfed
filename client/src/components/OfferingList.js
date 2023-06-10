644d2888d2411de2ec73338919497fbf1cf8b8df
Kate Bennert <kate.bennert@gmail.com>
2023-06-10 16:43:42 -0400

    Fix grid layout for offerings

--- client/src/components/OfferingList.js
+++ client/src/components/OfferingList.js
@@ -1,20 +1,29 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+// Update state on bids so that it shows up in the offering list!!!!
+
 function OfferingList({ offerings }) {
 
     return (
-        <> 
-            {offerings.length > 0 ? (
+        <div className="offering-card-container"> 
+            {(offerings.length > 0 && offerings !== []) ? (
                 offerings.map((offering) => (
-                    <div key={offering.id}>
-                        <h3>{offering.title}</h3>
-                        <p>listed by: {offering.user.username}</p>
-                        <img src={offering.image_url} alt={offering.title}/>
-                        <p>Description: {offering.description}</p>
-                        <p>Condition: {offering.condition}</p>
-                        <p>Category: {offering.category_tag}</p>
-                        <NavLink to={`/offerings/${offering.id}`}><button>View Offering</button></NavLink>
+                    <div key={offering.id} className="offering-card">
+                        <div className="offering-card-left">
+                            <h2 className="offering-title">{offering.title}</h2>
+                            <p>listed by: {offering.user.username}</p>
+                            <img src={offering.image_url} alt={offering.title}/>
+                        </div>
+                        <div className="offering-info">
+                            <p className="offering-description">Description: {offering.description}</p>
+                            <p className="offering-condition">Condition: {offering.condition}</p>
+                            <p className="offering-category-tag">Category: {offering.category_tag}</p>
+                            <p>Bids on this offer: {offering.bids.length}</p>
+                        </div>
+                        <div className="offering-card-buttons">
+                            <NavLink to={`/offerings/${offering.id}`}><button>View Offering</button></NavLink>
+                        </div>
                     </div>
                 ))
             ) : (
@@ -23,7 +32,7 @@ function OfferingList({ offerings }) {
                     <NavLink to={`/new-offering`}><button>Make a New Offering</button></NavLink>
                 </>
             )}
-        </>
+        </div>
     )
 }
 
