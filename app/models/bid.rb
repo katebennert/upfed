edddbdd98d5f489257bbceaba8f843cad1444982
Kate Bennert <kate.bennert@gmail.com>
2023-06-21 10:07:16 -0400

    Add new seeds

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -7,4 +7,9 @@ class Bid < ApplicationRecord
   validates :image_url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp, message: "must be a valid URL" }
   validates :condition, presence: true
   validates :category_tag, presence: true
+
+  def start_with_c
+    self.condition.first === "c"
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts 'Seeding 🌱'
 
-# locations = ["New York", "Jersey City", "Seattle", "Atlanta", "Denver", "San Antonio"]
-# conditions = ["Gently Used", "Well-Loved", "Heirloom", "Antique", "WANTED", "Slightly damaged"]
-# descriptions = ["Free to a good home", "(batteries not included)", "As is", "Like new", "(assembly required)", "No returns!!!!"]
-# titles = ["Batman Toy", "basketball", "Toddler Shoes Size 4", "Soccer Cleats", "Softball Bat"]
-# category_tags = ["clothing", "toys", "sports gear", "books", "shoes", "newborn"]
+locations = ["New York", "Jersey City", "Seattle", "Atlanta", "Denver", "San Antonio"]
+conditions = ["Gently Used", "Used", "Brand New", "Well Loved"]
+descriptions = ["Free to a good home", "(batteries not included)", "As is", "Like new", "(assembly required)", "No returns!!!!"]
+titles = ["Batman Toy", "basketball", "Toddler Shoes Size 4", "Soccer Cleats", "Softball Bat"]
+category_tags = ["clothing", "toys and games", "sports gear", "books and comics", "newborn and babies"]
 
-# 5.times do
-#     user = User.create(
-#       username: Faker::Internet.username,
-#       password: Faker::Internet.password,
-#       image_url: Faker::Avatar.image,
-#       bio: Faker::Lorem.sentence,
-#       location: locations.sample,
-#     )
-  
-#     rand(2..4).times do
-#       user.offerings.create(
-#         title: titles.sample,
-#         description: descriptions.sample,
-#         image_url: Faker::LoremFlickr.image(size: "300x300", search_terms: ['toys']),
-#         condition: conditions.sample,
-#         category_tag: category_tags.sample
-#       )
-#     end
-#   end
+10.times do
+    user = User.create({
+      username: Faker::Internet.username,
+      password: Faker::Internet.password,
+      image_url: Faker::Avatar.image,
+      bio: Faker::Lorem.sentence,
+      location: locations.sample,
+    })
+end
+
+20.times do
+    offering = Offering.create({
+        title: titles.sample,
+        description: descriptions.sample,
+        image_url: Faker::LoremFlickr.image(size: "300x300", search_terms: ['toys']),
+        condition: conditions.sample,
+        category_tag: category_tags.sample,
+        user_id: rand(1..10)
+    })
+end
+
+50.times do
+    bid = Bid.create({
+        title: titles.sample,
+        description: Faker::Lorem.paragraph,
+        image_url: Faker::LoremFlickr.image(size: "300x300", search_terms: ['toys']),
+        condition: conditions.sample,
+        category_tag: category_tags.sample,
+        message: Faker::Lorem.sentence,
+        user_id: rand(1..10),
+        offering_id: rand(1..20)
+    })
+end
+
+puts 'Seeding complete! ✅'
